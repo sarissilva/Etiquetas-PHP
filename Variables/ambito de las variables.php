f1675a62f20c0bdd36c75f5b0566dbdbd79da852
sara silva <smsara@misena.edu.co>
2016-05-08 11:35:10 -0500

    Variables

---
+++ Variables/ambito de las variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+$a=1;
+$b=2;
+
+function Suma()
+{
+    global $a, $b;
+
+    $b = $a + $b;
+}
+
+Suma();
+echo $b;
+?>
+
+</body>
+</html>
---
+++ Variables/variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+$var = 'Roberto';
+$Var = 'Juan';
+echo "$var, $Var";      // imprime "Roberto, Juan"
+
+$4site = 'aun no';      // inválido; comienza con un número
+$_4site = 'aun no';     // válido; comienza con un carácter de subrayado
+$täyte = 'mansikka';    // válido; 'ä' es ASCII (Extendido) 228
+?>
+
+
+</body>
+</html>
