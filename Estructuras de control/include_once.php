4d7cfebf41be6a2361a40df6b47b01532801a441
sara silva <smsara@misena.edu.co>
2016-05-08 11:10:54 -0500

    Estructuras de control

---
+++ Estructuras de control/include_once.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+include_once "a.php"; // esto incluirá a.php
+include_once "A.php"; // esto incluirá a.php ¡otra vez! (sólo PHP 4)
+?>
+</body>
+</html>
---
+++ Estructuras de control/requiere_once.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+echo "Hello";
+?>
+
+test.php
+<?php
+require('echo.php');
+require_once('echo.php');
+?>
+</body>
+</html>
